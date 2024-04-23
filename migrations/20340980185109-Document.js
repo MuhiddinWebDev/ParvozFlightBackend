f0af1cd20225f48e921bc9e602036ce381ad688d
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2024-04-23 19:03:02 +0500

    fixed

---
+++ migrations/20340980185109-Document.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable('document', {
+        id: {
+          autoIncrement: true,
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        title_uz: {
+          type: Sequelize.DataTypes.STRING(256),
+          allowNull: true,
+        },
+        title_ru: {
+          type: Sequelize.DataTypes.STRING(256),
+          allowNull: true,
+        },
+        title_ka: {
+          type: Sequelize.DataTypes.STRING(256),
+          allowNull: true,
+        },
+        file: {
+          type: Sequelize.DataTypes.STRING(256),
+          allowNull: true,
+        },
+        status: {
+          type: Sequelize.DataTypes.BOOLEAN,
+          allowNull: true,
+        },
+
+        createdAt: {
+          type: Sequelize.DataTypes.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DataTypes.DATE
+        },
+        deletedAt: {
+          type: Sequelize.DataTypes.DATE,
+        },
+      }, { transaction }
+      );
+
+      transaction.commit();
+    } catch (errors) {
+      transaction.rollback();
+      throw errors;
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.dropTable('document', { transaction });
+      transaction.commit();
+    } catch (errors) {
+      transaction.rollback();
+      throw errors;
+    }
+  }
+};
---
+++ src/models/document.model.js
@@ -0,0 +1,48 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../db/db-sequelize');
+class DocumentModel extends Model {
+  toJSON() {
+    let values = Object.assign({}, this.get());
+    delete values.createdAt;
+    delete values.updatedAt;
+    delete values.deletedAt;
+    return values;
+  }
+}
+
+DocumentModel.init({
+  id: {
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  title_uz: {
+    type: DataTypes.STRING(256),
+    allowNull: true,
+  },
+  title_ru: {
+    type: DataTypes.STRING(256),
+    allowNull: true,
+  },
+  title_ka: {
+    type: DataTypes.STRING(256),
+    allowNull: true,
+  },
+  file: {
+    type: DataTypes.STRING(256),
+    allowNull: true,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+  },
+}, {
+  sequelize,
+  modelName: 'DocumentModel',
+  tableName: 'document',
+  timestamps: true,
+  paranoid: true,
+});
+
+module.exports = DocumentModel;

7245ed86db6202c868546061fe8479bb5ec9af33
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2024-05-22 16:58:00 +0500

    fixed

--- src/middleware/clientAuth.middleware.js
+++ src/middleware/clientAuth.middleware.js
@@ -22,7 +22,6 @@ const auth = () => {
 
             // if the client has permissions
             req.currentClient = client;
-            console.log(client)
             next();
 
         } catch (e) {

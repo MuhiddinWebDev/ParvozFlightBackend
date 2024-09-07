cb8906accb81770f4e73424902c927f0d0ea0e6f
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2024-09-07 14:23:04 +0500

    fixed

--- src/middleware/validators/staticOrderValidator.middleware.js
+++ src/middleware/validators/staticOrderValidator.middleware.js
@@ -2,15 +2,15 @@ const Joi = require('joi');
 
 exports.orderSchemas = { 
   byClient: Joi.object({
-    passport: Joi.string().required(),
-    migrant_carta: Joi.string().required(),
-    phone: Joi.string().required()
+    passport: Joi.string().required().allow(null),
+    migrant_carta: Joi.string().required().allow(null),
+    phone: Joi.string().required().allow(null)
   }),
   byAdmin: Joi.object({
     status: Joi.string().required(),
     region_id: Joi.number().required(),
-    passport: Joi.string().required(),
-    migrant_carta: Joi.string().required(),
-    phone: Joi.string().required(),
+    passport: Joi.string().required().allow(null),
+    migrant_carta: Joi.string().required().allow(null),
+    phone: Joi.string().required().allow(null),
   })
 };
